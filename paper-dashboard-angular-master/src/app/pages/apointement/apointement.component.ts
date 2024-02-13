584734237e160ec67a6a278a735b332b4164b913
TokyNy <nyainatoky2001@gmail.com>
2024-02-13 05:51:01 +0300

    Apointement User

---
+++ paper-dashboard-angular-master/src/app/pages/apointement/apointement.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'apointement-cmp',
+    moduleId: module.id,
+    templateUrl: 'apointement.component.html'
+})
+
+export class ApointementComponent implements OnInit{
+    ngOnInit(){
+    }
+}

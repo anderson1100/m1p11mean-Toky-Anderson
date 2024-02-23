60f71b209524a959bd444e55606f3735c8509220
Anderson Mahosinantenaina <andersonmahosi@gmail.com>
2024-02-23 18:43:52 +0300

    angular employe-component-

--- angular/src/app/employe-calendar/employe-calendar.component.ts
+++ angular/src/app/employe-calendar/employe-calendar.component.ts
@@ -1,22 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-employe-calendar',
   standalone: true, 
   imports: [
-    FullCalendarModule,
     CommonModule,
-    FullCalendarModule,
     
   ],
   templateUrl: './employe-calendar.component.html',
   styleUrl: './employe-calendar.component.css'
 })
 export class EmployeCalendarComponent {
-  currentMonth: number = new Date().getMonth();
-  currentYear: number = new Date().getFullYear();
-  daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  daysInMonth = new Date(this.currentYear, this.currentMonth +  1,  0).getDate();
 }
